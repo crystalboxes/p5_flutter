43407eb025b10cec9dc75f9c4df82f27c4f557d5
crystalboxes <ursus95@gmail.com>
2020-07-17 16:25:05 +0300

    custom random

---
+++ lib/internal_random.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+
+class _CustomRandom {
+  Random _internal;
+  _CustomRandom([int seed]) {
+    _internal = Random(seed);
+  }
+
+  bool nextBool() => _internal.nextBool();
+
+  double nextDouble() => _internal.nextDouble();
+
+  int nextInt(int max) => _internal.nextInt(max);
+
+  double nextNextGaussian;
+  bool haveNextNextGaussian = false;
+
+  double nextGaussian() {
+    // See Knuth, ACP, Section 3.4.1 Algorithm C.
+    if (haveNextNextGaussian) {
+      haveNextNextGaussian = false;
+      return nextNextGaussian;
+    } else {
+      double v1, v2, s;
+      do {
+        v1 = 2 * nextDouble() - 1; // between -1 and 1
+        v2 = 2 * nextDouble() - 1; // between -1 and 1
+        s = v1 * v1 + v2 * v2;
+      } while (s >= 1 || s == 0);
+      double multiplier = sqrt(-2 * log(s) / s);
+      nextNextGaussian = v2 * multiplier;
+      haveNextNextGaussian = true;
+      return v1 * multiplier;
+    }
+  }
+
+  void setSeed(int seed) {
+    _internal = Random(seed);
+  }
+}
+
+class InternalRandom {
+  factory InternalRandom._() => null;
+  _CustomRandom internalRandom;
+
+  double random(double high) {
+    // avoid an infinite loop when 0 or NaN are passed in
+    if (high == 0 || high != high) {
+      return 0;
+    }
+
+    if (internalRandom == null) {
+      internalRandom = _CustomRandom();
+    }
+
+    // for some reason (rounding error?) Math.random() * 3
+    // can sometimes return '3' (once in ~30 million tries)
+    // so a check was added to avoid the inclusion of 'howbig'
+    double value = 0;
+    do {
+      value = internalRandom.nextDouble() * high;
+    } while (value == high);
+    return value;
+  }
+
+  /**
+     * Returns a float from a random series of numbers having a mean of 0
+     * and standard deviation of 1. Each time the <b>randomGaussian()</b>
+     * function is called, it returns a number fitting a Gaussian, or
+     * normal, distribution. There is theoretically no minimum or maximum
+     * value that <b>randomGaussian()</b> might return. Rather, there is
+     * just a very low probability that values far from the mean will be
+     * returned; and a higher probability that numbers near the mean will
+     * be returned.
+     *
+     * ( end auto-generated )
+     * @webref math:random
+     * @see PApplet#random(float,float)
+     * @see PApplet#noise(float, float, float)
+     */
+  double randomGaussian() {
+    if (internalRandom == null) {
+      internalRandom = new _CustomRandom();
+    }
+    return internalRandom.nextGaussian();
+  }
+
+  void randomSeed(int seed) {
+    if (internalRandom == null) {
+      internalRandom = new _CustomRandom();
+    }
+    internalRandom.setSeed(seed);
+  }
+}
